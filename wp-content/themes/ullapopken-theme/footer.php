e74d3d8900b8965fafaace1a660d98e9617ce3f8
Tharindu Hettiarachchi <hettipower@gamil.com>
2021-08-12 22:23:22 +0530

    Update

--- wp-content/themes/ullapopken-theme/footer.php
+++ wp-content/themes/ullapopken-theme/footer.php
@@ -1,5 +1,23 @@
 <footer>
-	
+	<div class="serviceWrap">
+        <div class="container d-flex justify-content-between">
+            
+            <div class="service"><i class="fas fa-certificate"></i></div>
+
+        </div>
+    </div>
+    <div class="middleWrap">
+        <div class="container d-flex justify-content-between">
+            <div class="footerBoxWrap"></div>
+            <div class="footerBoxWrap"></div>
+            <div class="footerBoxWrap"></div>
+        </div>
+    </div>
+    <div class="bottomWrap">
+        <div class="container d-flex justify-content-between">
+            <div class="quickMenu"></div>
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
--- wp-content/themes/ullapopken-theme/header.php
+++ wp-content/themes/ullapopken-theme/header.php
@@ -33,32 +33,78 @@
 <body <?php body_class(); ?>>
 
 	<header>
-		<!-- Static navbar -->
-		<nav class="navbar navbar-expand-md mb-4">
-	    	<a class="navbar-brand" href="<?php echo home_url(); ?>">
-				<?php
-					$site_logo = get_field( 'site_logo', 'option' ); 
-				  	if( $site_logo ): 
-				?>
-	          		<img src="<?php echo $site_logo['url']; ?>" alt="<?php echo $site_logo['alt']; ?>" />
-	          	<?php endif; ?>
-          	</a>
-	      	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-	        	<span class="navbar-toggler-icon"></span>
-	      	</button>
-	      	<div class="collapse navbar-collapse" id="navbarCollapse">
-	      		<?php
-	            	$defaults = array(
-		                'menu'            => 'Main Menu',
-		                'container'       => false,
-		                'menu_class'      => 'menu',
-		                'echo'            => true,
-		                'fallback_cb'     => 'wp_page_menu',
-		                'items_wrap'      => '<ul id="%1$s" class="%2$s navbar-nav mr-auto">%3$s</ul>',
-		                'depth'           => 0
-	              	);
-	              	wp_nav_menu( $defaults );
-	            ?>
-	      	</div>
+		<nav class="navbar navbar-expand-md">
+			<div class="topnavvbar">
+				<div class="container d-flex justify-content-end">
+					<div class="lang"></div>
+					<div class="menuWrap">
+						<?php
+							$defaults = array(
+								'menu'            => 'Top Menu',
+								'container'       => false,
+								'menu_class'      => 'menu',
+								'echo'            => true,
+								'fallback_cb'     => 'wp_page_menu',
+								'items_wrap'      => '<ul id="%1$s" class="%2$s navbar-nav">%3$s</ul>',
+								'depth'           => 0
+							);
+							wp_nav_menu( $defaults );
+						?>
+					</div>
+				</div>
+			</div>
+			<div class="middleWrap">
+				<div class="container d-flex justify-content-between">
+					<div class="topCatWrap">
+
+					</div>
+					<a class="navbar-brand" href="<?php echo home_url(); ?>">
+						<?php
+							$site_logo = get_field( 'site_logo', 'option' ); 
+							if( $site_logo ): 
+						?>
+							<img src="<?php echo $site_logo['url']; ?>" alt="<?php echo $site_logo['alt']; ?>" />
+						<?php endif; ?>
+					</a>
+					<ul class="secondMenuWrap navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#"><i class="far fa-user"></i><span>Sign In</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#"><i class="fas fa-clipboard-list"></i><span>Quick Order</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#"><i class="far fa-heart"></i><span>Favorites</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#"><i class="fas fa-shopping-bag"></i><span>Bag</span></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="bottomWrap">
+				<div class="container d-flex justify-content-between">
+					<div class="mainMenuWrap">
+
+					</div>
+					<div class="searchWrap">
+						<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+							<div class="input-group">
+								<input 
+									type="search" 
+									class="form-control"
+									placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>"
+									value="<?php echo get_search_query() ?>" name="s" 
+								>
+								<span class="input-group-text">
+									<button type="submit"><i class="fas fa-search"></i></button>
+								</span>
+							</div>
+							<input type="hidden" value="product" name="post_type" id="post_type" />
+						</form>
+					</div>
+				</div>
+			</div>
 	    </nav>
+		<div class="saleWrap"></div>
 	</header>
