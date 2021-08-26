a57a6a2bc57b868271f8efc4acc71abdfcf56b05
Tharindu Hettiarachchi <hettipower@gamil.com>
2021-08-26 23:09:54 +0530

    add to cart function update

--- wp-content/themes/ullapopken-theme/woocommerce/woo_config.php
+++ wp-content/themes/ullapopken-theme/woocommerce/woo_config.php
@@ -330,3 +330,32 @@ function header_add_to_cart_fragment( $fragments ) {
 
     return $fragments;
 }
+
+add_action( 'wp_ajax_woocommerce_add_to_cart_variable_rc', 'woocommerce_add_to_cart_variable_rc_callback' );	
+function woocommerce_add_to_cart_variable_rc_callback() {
+    $product_id = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $_POST['product_id'] ) );
+    $quantity = empty( $_POST['quantity'] ) ? 1 : apply_filters( 'woocommerce_stock_amount', $_POST['quantity'] );
+    $variation_id = $_POST['variation_id'];
+    $variation  = $_POST['variation'];
+    $passed_validation = apply_filters( 'woocommerce_add_to_cart_validation', true, $product_id, $quantity );
+
+    if ( $passed_validation && WC()->cart->add_to_cart( $product_id, $quantity, $variation_id, $variation  ) ) {
+        do_action( 'woocommerce_ajax_added_to_cart', $product_id );
+        if ( get_option( 'woocommerce_cart_redirect_after_add' ) == 'yes' ) {
+            wc_add_to_cart_message( $product_id );
+        }
+
+        // Return fragments
+        WC_AJAX::get_refreshed_fragments();
+    } else {
+        WC_AJAX::json_headers();
+
+        // If there was an error adding to the cart, redirect to the product page to show any errors
+        $data = array(
+            'error' => true,
+            'product_url' => apply_filters( 'woocommerce_cart_redirect_after_error', get_permalink( $product_id ), $product_id )
+        );
+        echo json_encode( $data );
+    }
+    die();
+}
