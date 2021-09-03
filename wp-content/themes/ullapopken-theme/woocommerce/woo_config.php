8cc628b747a4847bcfc4cd0a1163e4ea6bde139f
Tharindu Hettiarachchi <hettipower@gamil.com>
2021-09-04 02:08:24 +0530

    my accout pages

--- wp-content/themes/ullapopken-theme/woocommerce/woo_config.php
+++ wp-content/themes/ullapopken-theme/woocommerce/woo_config.php
@@ -455,3 +455,46 @@ function get_product_count($termSlug = '') {
     return $products->found_posts;
 
 }
+
+add_action( 'woocommerce_register_form_start', 'bbloomer_add_name_woo_account_registration' );
+function bbloomer_add_name_woo_account_registration() {
+    ?>
+  
+    <p class="form-row">
+        <label for="reg_billing_first_name"><?php _e( 'First name', 'woocommerce' ); ?> <span class="required">*</span></label>
+        <input type="text" class="input-text form-control" required name="billing_first_name" id="reg_billing_first_name" value="<?php if ( ! empty( $_POST['billing_first_name'] ) ) esc_attr_e( $_POST['billing_first_name'] ); ?>" />
+    </p>
+  
+    <p class="form-row">
+        <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
+        <input type="text" class="input-text form-control" required name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
+    </p>
+  
+    <div class="clear"></div>
+  
+    <?php
+}
+
+add_filter( 'woocommerce_registration_errors', 'bbloomer_validate_name_fields', 10, 3 );
+function bbloomer_validate_name_fields( $errors, $username, $email ) {
+    if ( isset( $_POST['billing_first_name'] ) && empty( $_POST['billing_first_name'] ) ) {
+        $errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required!', 'woocommerce' ) );
+    }
+    if ( isset( $_POST['billing_last_name'] ) && empty( $_POST['billing_last_name'] ) ) {
+        $errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!.', 'woocommerce' ) );
+    }
+    return $errors;
+}
+
+add_action( 'woocommerce_created_customer', 'bbloomer_save_name_fields' );
+function bbloomer_save_name_fields( $customer_id ) {
+    if ( isset( $_POST['billing_first_name'] ) ) {
+        update_user_meta( $customer_id, 'billing_first_name', sanitize_text_field( $_POST['billing_first_name'] ) );
+        update_user_meta( $customer_id, 'first_name', sanitize_text_field($_POST['billing_first_name']) );
+    }
+    if ( isset( $_POST['billing_last_name'] ) ) {
+        update_user_meta( $customer_id, 'billing_last_name', sanitize_text_field( $_POST['billing_last_name'] ) );
+        update_user_meta( $customer_id, 'last_name', sanitize_text_field($_POST['billing_last_name']) );
+    }
+  
+}
