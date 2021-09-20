9255258ae1bc82de61222c2b174075b9dbfedb58
Tharindu Hettiarachchi <hettipower@gamil.com>
2021-09-21 00:24:58 +0530

    multiple variation images plugin added

--- wp-content/themes/ullapopken-theme/woocommerce/woo_config.php
+++ wp-content/themes/ullapopken-theme/woocommerce/woo_config.php
@@ -30,8 +30,8 @@ function ullapopken_woo_add_woocommerce_support()
     ));
 
     //add_theme_support( 'wc-product-gallery-zoom' );
-    //add_theme_support('wc-product-gallery-lightbox');
-    //add_theme_support('wc-product-gallery-slider');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
 }
 
 add_action('woocommerce_before_shop_loop' , 'ullapopken_filter_btns_func' , 35);
