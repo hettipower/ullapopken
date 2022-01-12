f57c6ba2042f2f7cab84b101091bb0afb08bb65b
hettipower <hettipower@gmail.com>
2022-01-12 08:35:58 +0530

    wp update latest

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'ullapopken' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'maya' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
